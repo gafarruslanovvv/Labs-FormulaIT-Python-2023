8fa63bf09bf72058152e50d021971756a34b61c8
gafarruslanovvv <147272223+gafarruslanovvv@users.noreply.github.com>
2023-11-10 11:29:57 +0500

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,6 +1,23 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+import json
 
-# TODO заменить значение пропущенного элемента средним арифметическим
-sum_numbers = sum(numbers[0:4]) + sum(numbers[5:21])
-numbers[4] = sum_numbers / len(numbers)
-print("Измененный список:", numbers)
+
+def sum_of_products(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    sum_of_products = 0
+    for item in data:
+        score = item['score']
+        weight = item['weight']
+        product = score * weight
+        sum_of_products += product
+
+    return round(sum_of_products, 3)
+
+
+file_path = 'input.json'  # путь к файлу JSON
+result = sum_of_products(file_path)
+print(result)
+
+
+
--- task_2.py
+++ task_2.py
@@ -1,12 +1,29 @@
-# TODO Найдите количество книг, которое можно разместить на дискете
-bytes_ = 4
+import csv
+import json
 
-pages = 100
-lines = 50
-chars = 25
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
 
-total_chars = pages * lines * chars
-total_bytes = total_chars * bytes_
-disket = 1.44 * 1024 * 1024   # емкость дискеты в байтах
-x = int(disket / total_bytes)
-print("Количество книг, помещающихся на дискету:", x)
+
+def task() -> None:
+    with open(INPUT_FILENAME) as input_f:
+        reader = csv.DictReader(input_f, delimiter=',', lineterminator='\n')
+
+        records = []
+
+        for row in reader:
+            record = {}
+            for column, value in row.items():
+                record[column] = value
+            records.append(record)
+
+        with open(OUTPUT_FILENAME, 'w') as output_f:
+            json.dump(records, output_f, indent=4)
+
+
+if __name__ == '__main__':
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
